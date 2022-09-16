1cb783b942157ce7d0735d6291aa68d2b3611147
Sergei <60666684+sambelony@users.noreply.github.com>
2022-09-16 15:16:45 +0200

    Add files via upload

--- list-2.py
+++ list-2.py
@@ -26,3 +26,13 @@ def sum13(nums):
     else:
       sums += nums[i]
   return sums
+
+"""Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7
+(every 6 will be followed by at least one 7). Return 0 for no numbers."""
+def sum67(nums):
+  i = 0
+  numbers_between = 0
+  while 6 in nums[i:] and 7 in nums[i:]:
+    numbers_between += sum(nums[i:][nums[i:].index(6):nums[i:].index(7) + 1:])
+    i = nums.index(7, i) + 1
+  return sum(nums) - numbers_between
